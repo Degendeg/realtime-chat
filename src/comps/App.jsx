0dd04ca587e1b18b9bf6a19cecb13b5aed1075a0
Sebastian <sebastian.degerman@consid.se>
2024-11-25 20:58:24 +0100

    add logout svg

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -73,6 +73,11 @@ function App() {
 
   return (
     <div className={`p-5 ${!isMobile ? 'mt-40' : 'mt-20'}`}>
+      <svg onClick={() => { localStorage.clear(); location.pathname = '/'; }} className="h-8 w-8 cursor-pointer text-gray-500 absolute top-0 left-0 mt-14 ml-14" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+        <path d="M7 12h14l-3 -3m0 6l3 -3" />
+      </svg>
       <input type="checkbox" value="retro" className="absolute top-0 right-0 toggle theme-controller mt-14 mr-14"
         onChange={() => setDarkTheme(darkTheme => !darkTheme)} />
       <Keyboard />
